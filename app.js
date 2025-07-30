9921dcd437c166526b3c085460b48c7bf344051f
Muhammad041008 <mohdimran041008@gmail.com>
2025-07-30 23:47:58 +0530

    Your commit message

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ app.use(session({
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
 })
     .then(() => console.log("Connected to MongoDB"))
     .catch((error) => console.error("MongoDB connection error:", error));
